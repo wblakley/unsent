d8829170cca7a1af7e3e3f5ac570a89477dbf51b
wblakely29 <alicemaedllc@gmail.com>
2026-01-08 11:53:04 -0500

    Fix letters route dynamic rendering and client component config

--- src/app/letters/LettersClient.tsx
+++ src/app/letters/LettersClient.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
 
 import React, { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
--- src/app/letters/page.tsx
+++ src/app/letters/page.tsx
@@ -1,8 +1,6 @@
-"use client";
+import LettersClient from "./LettersClient";
 
 export const dynamic = "force-dynamic";
-
-import LettersClient from "./LettersClient";
 
 export default function Page() {
   return <LettersClient />;
